ed4373332406563c1b7a1191722ba73c2893c0d3
Roua Messaoudi <rouamessaoudi90@gmail.com>
2026-03-06 01:34:56 +0100

    Config Server pour toute la plateforme - TP

---
+++ backend/backend/backEnd/microservices/courses/src/main/java/com/esprit/courses/controllers/WelcomeController.java
@@ -0,0 +1,23 @@
+package com.esprit.courses.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "${cors.allowed-origins}")
+public class WelcomeController {
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return welcomeMessage;
+    }
+}
